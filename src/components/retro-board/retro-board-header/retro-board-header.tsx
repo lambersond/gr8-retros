486d625d1d1eb7b495d536e56bb8f3a7a1a507aa
David Lamberson <lamberson.david.ray@gmail.com>
2025-12-14 17:24:10 -0500

    fix: retro header height

--- src/components/retro-board/retro-board-header/retro-board-header.tsx
+++ src/components/retro-board/retro-board-header/retro-board-header.tsx
@@ -3,7 +3,7 @@ import { Countdown } from '@/components/countdown'
 
 export function RetroBoardHeader({ id }: Readonly<{ id: string }>) {
   return (
-    <div className='bg-appbar/50 py-2 px-3 relative'>
+    <div className='bg-appbar/50 py-2 px-3 h-13 relative'>
       <Countdown id={id} />
       <RetroActions id={id} />
     </div>
