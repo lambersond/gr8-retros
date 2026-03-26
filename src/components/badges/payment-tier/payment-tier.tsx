2c65d93d838960abd5e6d63a559d22d9fbb442cc
David Lamberson <lamberson.david.ray@gmail.com>
2026-03-26 15:57:16 -0400

    fix: paymentTier bug

--- src/components/badges/payment-tier/payment-tier.tsx
+++ src/components/badges/payment-tier/payment-tier.tsx
@@ -2,7 +2,9 @@ import clsx from 'classnames'
 import { PAYMENT_TIER_META } from '@/constants'
 import { PaymentTier as PaymentTierEnum } from '@/enums'
 
-export function PaymentTier({ tier }: Readonly<{ tier: PaymentTierEnum }>) {
+export function PaymentTier({
+  tier = PaymentTierEnum.FREE,
+}: Readonly<{ tier?: PaymentTierEnum }>) {
   const tierMeta = PAYMENT_TIER_META[tier]
   const classes = clsx(
     'text-[10px] font-semibold px-2 py-0.5 rounded-full border',
